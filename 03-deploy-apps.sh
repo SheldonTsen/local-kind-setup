f8d03377c30aa1e04490e9d1734f0ee905bd2612
ST <stsen@eit.org>
2026-01-27 20:54:36 +0000

    fix plaintext

--- 03-deploy-apps.sh
+++ 03-deploy-apps.sh
@@ -10,8 +10,10 @@ kubectl apply -f argoworkflows/app.yaml
 # based off ingress configuration
 echo "Logging into Argo CD..."
 kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=120s
+
+
 # yes | argocd login argocd-login.localhost:8443 --insecure --username admin --password admin
-yes | argocd login argocd.localhost:8080 --insecure --username admin --password admin
+yes | argocd login argocd.localhost:8080 --insecure --username admin --password admin --plaintext
 
 APP_NAME="argo-workflows"
 NAMESPACE="argocd"
